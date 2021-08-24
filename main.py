1ada6184c7aca20c3847a71c5cc88933bb037f87
Jean-Emmanuel Longueville <jean.emmanuel.longueville@univ-poitiers.fr>
2021-08-24 16:53:28 +0200

    Back to all software

--- main.py
+++ main.py
@@ -84,79 +84,79 @@ def main(path: str = None, notes: str = None, sortie: str = None, echo = True, c
     except FileNotFoundError:
         print("ATTENTION! PAS DE Fichier Excel DÉTÉCTÉ!")
     print(f"On attaque la saisie du groupe {grp}")
-#     ######################################    MED    ######################################
-#     try:
-#         listd = ldir(f"{path}/med_associate")
-#         listd = [i for i in listd if i[0] != "."]
-#         nb_files = len(listd)
-#         for i in range(nb_files):
-#             dtsp = time()-tim_stamp
-#             tim_stamp = time()
-#             chn = f"""\n\nMED : {listd[i]} dossier {i}/{nb_files-1}    {"-"*i}{"."*(nb_files-i-1)}
-#             Temps restant estimé : {int(dtsp*(nb_files-i))//60}:{int(dtsp*(nb_files-i))%60} """
-#             print(chn+"\n"*5)
-#             listd2 = ldir(f"{path}/med_associate/{listd[i]}")
-#             listd2 = [i for i in listd2 if i[0] != "."]
-#             nb_files2 = len(listd2)
-#             for j in range(nb_files2):
-#                 dtsp = time()-tim_stamp
-#                 tim_stamp = time()
-#                 test="""\n\nMED : {} dossier {}/{}    {}{}
-# {} :  dossier {}/{}    {}{}
-# Temps restant estimé : {}:{}{}""".format(listd[i],i,nb_files-1,"-"*i,"."*(nb_files-i-1),listd[i],
-#                     j,nb_files2-1,"-"*j,"."*(nb_files2-j-1),int(dtsp*((nb_files2*nb_files)-i))//60,
-#                     int(dtsp*((nb_files2*nb_files)-i))%60,"\n"*2)
-#                 print(test)
-#                 try:
-#                     read_folder(f"{path}/med_associate/{listd[i]}/{listd2[j]}",dfanimals,con,0)
-#                 except Exception as err:
-#                     rep = input(
-#                         f"Un problème est survenu pendant le traitement des données: \n{err}\n \nVoulez-vous quand même continuer?")
-#                     if not(rep.replace(" ", "").lower() in ["oui", "yes", "o", "y"]):
-#                         raise RuntimeError(
-#                             f"Vous avez choisi d'arreter l'éxécution après l'erreur suivante:\n {err}")
-#     except FileNotFoundError as e:
-#         print(f"ATTENTION! PAS DE DOSSIER MED DÉTÉCTÉ!({e})")
+    ######################################    MED    ######################################
+    try:
+        listd = ldir(f"{path}/med_associate")
+        listd = [i for i in listd if i[0] != "."]
+        nb_files = len(listd)
+        for i in range(nb_files):
+            dtsp = time()-tim_stamp
+            tim_stamp = time()
+            chn = f"""\n\nMED : {listd[i]} dossier {i}/{nb_files-1}    {"-"*i}{"."*(nb_files-i-1)}
+            Temps restant estimé : {int(dtsp*(nb_files-i))//60}:{int(dtsp*(nb_files-i))%60} """
+            print(chn+"\n"*5)
+            listd2 = ldir(f"{path}/med_associate/{listd[i]}")
+            listd2 = [i for i in listd2 if i[0] != "."]
+            nb_files2 = len(listd2)
+            for j in range(nb_files2):
+                dtsp = time()-tim_stamp
+                tim_stamp = time()
+                test="""\n\nMED : {} dossier {}/{}    {}{}
+{} :  dossier {}/{}    {}{}
+Temps restant estimé : {}:{}{}""".format(listd[i],i,nb_files-1,"-"*i,"."*(nb_files-i-1),listd[i],
+                    j,nb_files2-1,"-"*j,"."*(nb_files2-j-1),int(dtsp*((nb_files2*nb_files)-i))//60,
+                    int(dtsp*((nb_files2*nb_files)-i))%60,"\n"*2)
+                print(test)
+                try:
+                    read_folder(f"{path}/med_associate/{listd[i]}/{listd2[j]}",dfanimals,con,0)
+                except Exception as err:
+                    rep = input(
+                        f"Un problème est survenu pendant le traitement des données: \n{err}\n \nVoulez-vous quand même continuer?")
+                    if not(rep.replace(" ", "").lower() in ["oui", "yes", "o", "y"]):
+                        raise RuntimeError(
+                            f"Vous avez choisi d'arreter l'éxécution après l'erreur suivante:\n {err}")
+    except FileNotFoundError as e:
+        print(f"ATTENTION! PAS DE DOSSIER MED DÉTÉCTÉ!({e})")
 
-#     ######################################    IMET    ######################################
-#     try:
-#         imetronic_insert(f"{path}/imetronic", dfanimals, con)
-#     except FileNotFoundError:
-#         print("ATTENTION! PAS DE DOSSIER IMET DÉTÉCTÉ!")
+    ######################################    IMET    ######################################
+    try:
+        imetronic_insert(f"{path}/imetronic", dfanimals, con)
+    except FileNotFoundError:
+        print("ATTENTION! PAS DE DOSSIER IMET DÉTÉCTÉ!")
 
-#     #############################     HotPlate / Openfield    #############################
-#     try:
-#         opfi = f"""{path}/openfield/{[file for file in ldir(f"{path}/openfield/") if file.endswith("XLS")][0]}"""
-#         xlsx_file = f"""{path}/{[file for file in ldir(path) if file.endswith("xlsx")][0]}"""
-#         insert_hp_of(path_openfile= opfi, path_excel= xlsx_file, dfanimals=dfanimals, con=con)
-#     except FileNotFoundError:
-#         print("ATTENTION! PAS DE DOSSIER OPENFIELD DÉTÉCTÉ!")
+    #############################     HotPlate / Openfield    #############################
+    try:
+        opfi = f"""{path}/openfield/{[file for file in ldir(f"{path}/openfield/") if file.endswith("XLS")][0]}"""
+        xlsx_file = f"""{path}/{[file for file in ldir(path) if file.endswith("xlsx")][0]}"""
+        insert_hp_of(path_openfile= opfi, path_excel= xlsx_file, dfanimals=dfanimals, con=con)
+    except FileNotFoundError:
+        print("ATTENTION! PAS DE DOSSIER OPENFIELD DÉTÉCTÉ!")
 
-#     ######################################     IM    ######################################
-#     try:
-#         listd = ldir(f"{path}/IM")
-#         listd = [i for i in listd if i[0] != "."]
-#         nb_files = len(listd)
-#         dfanimalscopyIM = dfanimals[["RFID", "animal_name", "groupe", "animal_id"]]
-#         #dfanimalscopyIM = dfanimalscopyIM.rename({"animal_name":"name","animal_id":"id"}) #!DELETE THIS Later
-#         for i in range(nb_files):
-#             dtsp = time()-tim_stamp
-#             tim_stamp = time()
-#             chn = f"""\n\nIM : {listd[i]} dossier {i}/{nb_files-1}    {"-"*i} {"."*(nb_files-i-1)}
-# Temps restant estimé : {int(dtsp*(nb_files-i))//60}:{int(dtsp*(nb_files-i))%60} """
-#             print(chn+"\n"*5)
-#             try:
-#                 if IC.notempty_phw_file(f"{path}/IM/{listd[i]}/AntennaReader/Antenna.txt"):
-#                     dfanimalscopyIM = readfoldersessionIM(
-#                         f"{path}/IM/{listd[i]}/", con, dfanimalscopyIM, chn, sortie, notes)
-#             except Exception as err:
-#                 rep = input(
-#                     f"Un problème est survenu pendant le traitement des données: \n{err}\n \nVoulez-vous quand même continuer?")
-#                 if not(rep.replace(" ", "").lower() in ["oui", "yes", "o", "y"]):
-#                     raise RuntimeError(
-#                         f"Vous avez choisi d'arreter l'éxécution après l'erreur suivante:\n {err}")
-#     except FileNotFoundError:
-#         print("ATTENTION! PAS DE DOSSIER IM DÉTÉCTÉ!")
+    ######################################     IM    ######################################
+    try:
+        listd = ldir(f"{path}/IM")
+        listd = [i for i in listd if i[0] != "."]
+        nb_files = len(listd)
+        dfanimalscopyIM = dfanimals[["RFID", "animal_name", "groupe", "animal_id"]]
+        #dfanimalscopyIM = dfanimalscopyIM.rename({"animal_name":"name","animal_id":"id"}) #!DELETE THIS Later
+        for i in range(nb_files):
+            dtsp = time()-tim_stamp
+            tim_stamp = time()
+            chn = f"""\n\nIM : {listd[i]} dossier {i}/{nb_files-1}    {"-"*i} {"."*(nb_files-i-1)}
+Temps restant estimé : {int(dtsp*(nb_files-i))//60}:{int(dtsp*(nb_files-i))%60} """
+            print(chn+"\n"*5)
+            try:
+                if IC.notempty_phw_file(f"{path}/IM/{listd[i]}/AntennaReader/Antenna.txt"):
+                    dfanimalscopyIM = readfoldersessionIM(
+                        f"{path}/IM/{listd[i]}/", con, dfanimalscopyIM, chn, sortie, notes)
+            except Exception as err:
+                rep = input(
+                    f"Un problème est survenu pendant le traitement des données: \n{err}\n \nVoulez-vous quand même continuer?")
+                if not(rep.replace(" ", "").lower() in ["oui", "yes", "o", "y"]):
+                    raise RuntimeError(
+                        f"Vous avez choisi d'arreter l'éxécution après l'erreur suivante:\n {err}")
+    except FileNotFoundError:
+        print("ATTENTION! PAS DE DOSSIER IM DÉTÉCTÉ!")
 
     ######################################    IC    ######################################
     try:
