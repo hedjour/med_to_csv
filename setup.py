73852adbacb8683932cd290cc5fd3739c1d4d3d5
Myriam Hanna <myriam.hanna@univ-poitiers.fr>
2021-09-29 10:57:19 +0200

    first commit with setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name = "med_tocsv",
+    version = "1.0",
+    author = "Jean-Emmanuel Longueville",
+    author_email = "J-E@univ-poitiers.fr",
+    description = "A module that converts MedAssociates data to csv files",
+    license = "",
+    url = "https://sourcesup.renater.fr/anonscm/git/medanalysis/medanalysis.git",
+    packages= find_packages(),
+    install_requires=['pandas',
+                      'numpy',
+                      'argparse'],
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: ",
+    ],
+    entry_points = {
+        'console_scripts':  ['med_tocsv-run = med_project.expe_med_to_csv:main']
+    }
+)
